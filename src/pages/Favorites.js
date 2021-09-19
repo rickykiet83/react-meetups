a2d81864fdaaa639d72b6e72acef0742c2bede7a
Kiet Pham <kietpham.dev@gmail.com>
2021-09-19 23:35:27 +1000

    init project

---
+++ src/pages/Favorites.js
@@ -0,0 +1,23 @@
+import FavoritesContext from '../store/favorites-context';
+import MeetupList from '../components/meetups/MeetupList';
+import { useContext } from 'react';
+
+function FavoritesPage() {
+  const favoriteCtx = useContext(FavoritesContext);
+
+  let content;
+  if (favoriteCtx.totalFavorites === 0) {
+    content = <p>You got no favorites yet. Start adding some?</p>;
+  } else {
+    content = <MeetupList meetups={favoriteCtx.favorites} />;
+  }
+
+  return (
+    <section>
+      <h1>My Favorites</h1>
+      {content}
+    </section>
+  );
+}
+
+export default FavoritesPage;
